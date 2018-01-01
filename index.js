5a478f0d3d1675c09f9f2b631c9727a04a64c849
CalvinTse <sam.calvin.tse@gmail.com>
2018-01-01 01:06:38 -0500

    debug postback FB messenger Issue can solve on local

--- index.js
+++ index.js
@@ -377,7 +377,7 @@ app.post('/webhook', (req, res) => {
 
             // Gets the message. entry.messaging is an array, but 
             // will only ever contain one message, so we get index 0
-			console.log("webhookEvent " + entry);
+			console.log("webhookEvent " + entry.messaging);
             let webhookEvent = entry.messaging[0];
             console.log(webhookEvent);
 
